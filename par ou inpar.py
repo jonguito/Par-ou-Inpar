21c2acb3269efa2259c31f361347a502901ac065
jonga <jvfps20@gmail.com>
2024-08-06 00:15:16 -0300

    Create par ou inpar.py

---
+++ par ou inpar.py
@@ -0,0 +1,6 @@
+numero = int(input('Me diga um número qualquer: '))
+par_inpar =  numero % 2
+if par_inpar == 0 :
+    print(f'O número {numero} é PAR.')
+elif par_inpar != 0:
+    print(f'O número {numero} é ÍN
